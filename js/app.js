da10735bccbda9bdc83f205c43ad7175cf64e3fc
unknown <hamerpantoja@gmail.com>
2021-08-08 18:55:20 -0500

    operaciones basicas

--- js/app.js
+++ js/app.js
@@ -1,5 +1,26 @@
 var app = new Vue({
     el: '#app',
     data: {
+        num1: null,
+        num2: null,
+        rta: null
+         },
+         methods:{
+             sumar(){
+               this.rta = this.num1 + this.num2;
+                           },
+             restar(){
+             this.rta = this.num1 - this.num2;
+             },
+             multiplicar(){
+             this.rta = this.num1 * this.num2;
+             },
+             dividir(){
+               if (this.num2==0) {
+                   this.rta = "Error, no se puede dividir pr cero";
+               }else{
+                   this.rta=this.num1/this.num2
+               }
+             }
          }
   });
